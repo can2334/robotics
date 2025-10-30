28345237ac9f6b63ff72edea7dd33db7d7855f7e
can2334 <101044119+can2334@users.noreply.github.com>
2025-10-30 10:55:19 +0300

    Test update

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://va.vercel-scripts.com;
+script-src 'self' ' https://www.googletagmanager.com https://www.google-analytics.com https://va.vercel-scripts.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               img-src 'self' data: https:;
               font-src 'self' https://fonts.gstatic.com;
